3c486df05d1c095ad43f424dd257f4a0eb4371b2
Bhumika Gupta <bhumika1511gupt@gmail.com>
2026-07-06 23:50:37 +0530

    Oops project update - added Book class

---
+++ cognizant_DN5.0_Upskilling-main/Oops_Project/LibraryManagement/Book.java
@@ -0,0 +1,62 @@
+package librarymanagement;
+
+public class Book {
+
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    // Constructor
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    // Getters
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    // Issue Book
+    public void issueBook() {
+        if (!isIssued) {
+            isIssued = true;
+            System.out.println(title + " has been issued.");
+        } else {
+            System.out.println(title + " is already issued.");
+        }
+    }
+
+    // Return Book
+    public void returnBook() {
+        if (isIssued) {
+            isIssued = false;
+            System.out.println(title + " has been returned.");
+        } else {
+            System.out.println(title + " was not issued.");
+        }
+    }
+
+    // Display Book Details
+    public void displayBook() {
+        System.out.println("\nBook ID: " + bookId);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Status: " + (isIssued ? "Issued" : "Available"));
+    }
+}
