60f260ef5d40707c83d9a790a3873bd1f806bb46
Bhumika Gupta <bhumika1511gupt@gmail.com>
2026-07-06 23:27:42 +0530

    Improve readability of Leap Year Checker

--- cognizant_DN5.0_Upskilling-main/Module_3-Core_Java/Q4_LeapYearChecker.java
+++ cognizant_DN5.0_Upskilling-main/Module_3-Core_Java/Q4_LeapYearChecker.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
 
 public class Q4_LeapYearChecker {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a year: ");
-        int year = sc.nextInt();
-        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        System.out.println(year + " is " + (isLeap ? "a Leap Year" : "not a Leap Year"));
+        int year = scanner.nextInt();
+
+        // A leap year is divisible by 4 but not by 100,
+        // unless it is also divisible by 400.
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0)
+                || (year % 400 == 0);
+
+        if (isLeapYear) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+
+        scanner.close();
     }
 }
